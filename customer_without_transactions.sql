d2c8eacb77dec66f65a59717c5a505db5d2fb244
HariniMadasu <harinimadasu6@gmail.com>
2026-06-02 12:16:14 +0530

    Adding SQL query for customers without transactions
    Query retrieves customer IDs and counts visits without transactions.

---
+++ customer_without_transactions.sql
@@ -0,0 +1,8 @@
+-- Write a solution to find the IDs of the users who visited without making any transactions and the number of times they made these types of visits.
+
+select v.customer_id, count(v.visit_id) as count_no_trans
+from visits as v
+left join transactions as t 
+on v.visit_id = t.visit_id 
+where transaction_id is null
+group by v.customer_id
