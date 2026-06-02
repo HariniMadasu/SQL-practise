bda327835f77197f7fdae795915ceb9b1711289f
HariniMadasu <harinimadasu6@gmail.com>
2026-06-02 12:22:46 +0530

    Creating manager_report.sql for manager report query
    Add SQL query to find managers with at least five direct reports.

---
+++ manager_report.sql
@@ -0,0 +1,8 @@
+-- Write a solution to find managers with at least five direct reports.
+
+select e1.name
+from employee e1
+join employee e2
+    on e1.id = e2.managerid
+group by e2.managerid
+having count(e2.managerid)>=5
