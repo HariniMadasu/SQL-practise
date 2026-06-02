4ec743f6ba14b4b600166dab35680965dadb5d93
HariniMadasu <harinimadasu6@gmail.com>
2026-06-02 12:07:52 +0530

    Adding SQL query for authors viewing their own articles
    Query to find authors who viewed their own articles.

---
+++ article_views.sql
@@ -0,0 +1,7 @@
+-- Writing a solution to find all the authors that viewed at least one of their own articles.
+-- Return the result table sorted by id in ascending order.
+
+select distinct author_id as id
+from views
+where author_id = viewer_id
+order by author_id
