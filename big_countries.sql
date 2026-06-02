ca742ca30982149609c86ee26c67e67f05c57521
HariniMadasu <harinimadasu6@gmail.com>
2026-06-02 12:06:17 +0530

    Adding SQL query for big countries
    Query to select big countries based on area or population.

---
+++ big_countries.sql
@@ -0,0 +1,7 @@
+-- A country is considered to be big if:
+-- 1. it has an area of at least three million (i.e., 3000000 km2), or it has a population of at least twenty-five million (i.e., 25000000).
+-- Writing a solution to find the name, population, and area of the big countries.
+
+select name, population, area
+from World
+where area >= 3000000 or population >= 25000000
